83fd6e1faa5e5f8b35710db2a4c74a2e5dc2750f
ple <pleknosthe@gmail.com>
2020-10-19 17:30:53 +0300

    add styling to about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,6 +4,11 @@
     <h2>Стараюсь писать чистый код. Иногда получается.</h2>
     <p>Занимаюсь разработкой сайтов. Позиционирую себя как фронтендера, но достаточно много работал с Node.js.</p>
     <p>Мне доставляет некоторое удовольствие процесс изучения новых технологий, и, как следствие, я стараюсь не пропускать новинки в мире веб-разработки и хотя бы поверхностно, но знакомиться с ними.</p>
+    <p>Мои контакты:</p>
+    <ul>
+      <li><a href="tel:+79197730409" target="_blank">+7 (919) 773 04 09</a></li>
+      <li><a href="mailto:pleknosthe@gmail.com" target="_blank">pleknosthe@gmail.com</a></li>
+    </ul>
   </main>
 </template>
 
@@ -14,5 +19,12 @@ export default {
 </script>
 
 <style>
+main.about {
+  max-width: var(--screen-l);
+  padding: 0 2rem;
+}
 
+main.about a {
+  color: var(--color-darker);
+}
 </style>
